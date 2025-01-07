8d8be66db717289b550b45396f8603794f97cc99
Rusty Pickle <rusty.pickle94@gmail.com>
2025-01-08 05:07:40 +0600

    Format

--- funnel-shared/src/lib.rs
+++ funnel-shared/src/lib.rs
@@ -1,13 +1,13 @@
 pub mod guild_channel;
+pub mod member_activity;
 pub mod member_count;
 pub mod message_user;
 pub mod request;
 pub mod response;
-pub mod member_activity;
 
 pub use guild_channel::*;
+pub use member_activity::*;
 pub use member_count::*;
 pub use message_user::*;
 pub use request::*;
 pub use response::*;
-pub use member_activity::*;
